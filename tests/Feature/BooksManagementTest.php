3b8c2f0bde1f544b8b5ddd5d45381fabacfe0a57
Nadim Ouertani <nadimouertani@hotmail.com>
2022-05-21 22:26:40 +0400

    Add update testing

--- tests/Feature/BooksManagementTest.php
+++ tests/Feature/BooksManagementTest.php
@@ -8,8 +8,9 @@ use Tests\TestCase;
 
 class BooksManagementTest extends TestCase
 {
-    /** @test */
     use RefreshDatabase;
+    
+    /** @test */
     public function can_add_a_book()
     {
         $this->withoutExceptionHandling();
@@ -21,5 +22,48 @@ class BooksManagementTest extends TestCase
         $response->assertOk();
         $this->assertCount(1, Book::all());
 
+    }
+
+    /** @test */
+    public function a_title_is_required()
+    {
+//        $this->withoutExceptionHandling();
+        $response = $this->post('/books', [
+            'title'=>'',
+            'author'=>'nadim',
+        ]);
+
+        $response->assertSessionHasErrors('title');
+    }
+
+    /** @test */
+    public function an_author_is_required()
+    {
+//        $this->withoutExceptionHandling();
+        $response = $this->post('/books', [
+            'title'=>'testing book',
+            'author'=>'',
+        ]);
+
+        $response->assertSessionHasErrors('author');
+    }
+
+    /** @test */
+    public function a_book_can_be_updated()
+    {
+        $this->withoutExceptionHandling();
+        $this->post('/books', [
+            'title'=>'Testing book',
+            'author'=>'nadim',
+        ]);
+
+        $book = Book::first();
+
+        $response = $this->patch('/books/' . $book->id,[
+            'title'=>'Testing book updated',
+            'author'=>'nadim updated'
+        ]);
+        $this->assertEquals('Testing book updated', Book::first()->title);
+        $this->assertEquals('nadim updated', Book::first()->author);
     }
 }
