d4abd6ed3a36efe298fde44af961e505d7786dc1
Nadim Ouertani <nadimouertani@hotmail.com>
2022-05-22 01:24:17 +0400

    Add slug to path and year test

--- tests/Feature/BooksManagementTest.php
+++ tests/Feature/BooksManagementTest.php
@@ -16,16 +16,15 @@ class BooksManagementTest extends TestCase
         $response = $this->post('/books', $this->validData());
 
         $book = Book::first();
-
         $this->assertCount(1, Book::all());
-        $response->assertRedirect('/books/' . $book->id);
+        $response->assertRedirect($book->path());
 
     }
 
     /** @test */
     public function field_required()
     {
-        collect(['title','author'])->each(function($field) {
+        collect(['title','author', 'year'])->each(function($field) {
             $response = $this->post('/books', array_merge($this->validData(), [$field => '']));
             $response->assertSessionHasErrors($field);
             $this->assertCount(0, Book::all());
@@ -35,43 +34,33 @@ class BooksManagementTest extends TestCase
     /** @test */
     public function a_book_can_be_updated()
     {
-        $this->post('/books', [
-            'title'=>'Testing book',
-            'author'=>'nadim',
-        ]);
+        $this->post('/books', $this->validData());
 
         $book = Book::first();
-
-        $response = $this->patch('/books/' . $book->id,[
-            'title'=>'Testing book updated',
-            'author'=>'nadim updated'
-        ]);
-        $this->assertEquals('Testing book updated', Book::first()->title);
-        $this->assertEquals('nadim updated', Book::first()->author);
-        $response->assertRedirect('/books/' . $book->id);
+        $response = $this->patch($book->path(),array_merge($this->validData(), ['title'=> 'Updated title', 'author' => 'Updated author']));
+        $this->assertEquals('Updated title', Book::first()->title);
+        $this->assertEquals('Updated author', Book::first()->author);
+        $response->assertRedirect($book->fresh()->path());
     }
 
     /** @test */
     public function a_book_can_be_deleted()
     {
-        $this->withoutExceptionHandling();
-        $this->post('/books', [
-            'title'=>'Testing book',
-            'author'=>'nadim',
-        ]);
+        $this->post('/books', $this->validData());
 
         $book = Book::first();
         $this->assertCount(1, Book::all());
 
-        $response = $this->delete('/books/' . $book->id);
+        $response = $this->delete($book->path());
         $this->assertCount(0, Book::all());
-        $response->assertRedirect('/books/' . $book->id);
+        $response->assertRedirect('/books');
     }
 
     private function validData() {
         return [
             'title'=>'testing book',
             'author'=>'nadim',
+            'year' => '1991',
         ];
     }
 }
