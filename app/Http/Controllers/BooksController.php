fc048928951375c9e733f78ce625f916bacd1397
Nadim Ouertani <nadimouertani@hotmail.com>
2022-05-21 22:26:07 +0400

    Add update & refactoring the code

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -8,9 +8,22 @@ class BooksController extends Controller
 {
     public function store()
     {
-        Book::create([
-            'title' => request('title'),
-            'author'=> request('author'),
+        Book::create($this->validateRequest());
+    }
+
+    public function update(Book $book)
+    {
+        $book->update($this->validateRequest());
+    }
+
+    /**
+     * @return array
+     */
+    protected function validateRequest()
+    {
+        return request()->validate([
+            'title' => 'required',
+            'author' => 'required',
         ]);
     }
 }
