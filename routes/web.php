a4d3b5b426150a0db6dea5702afb7f3a84d21301
Nadim Ouertani <nadimouertani@hotmail.com>
2022-05-21 18:51:14 +0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/book', function () {
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::post('/book', function () {
     return view('welcome');
 });
