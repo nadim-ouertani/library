bb0de960045fcb0d91753d996fd530548b50e1c8
Nadim Ouertani <nadimouertani@hotmail.com>
2022-05-22 20:36:37 +0400

    Add Book factory

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
 class Book extends Model
 {
+    use HasFactory;
     protected $guarded = [];
 
     public function path()
