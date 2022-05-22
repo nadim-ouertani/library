cd13f4871a5ac835690a49f2527675c79ba1274a
Nadim Ouertani <nadimouertani@hotmail.com>
2022-05-22 21:11:32 +0400

    Rename route

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome.blade.php');
+    return view('welcome');
 });
 
 Route::post('/books',[ \App\Http\Controllers\BooksController::class, 'store']);
